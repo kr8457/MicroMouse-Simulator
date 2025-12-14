215782dd54be1e7a0dcc30da869692e2e1f5accb
Khalid <khalidrafique8457@gmail.com>
2025-12-14 14:27:03 +0500

    fix: kfkfklkdfld

--- src/MazeGen.cpp
+++ src/MazeGen.cpp
@@ -2,13 +2,7 @@
 
 std::string MazeGen::generate(size_t rows, size_t columns) {
     std::string maze = "";
-    for (size_t row = 0; row < rows; row++) {
-        maze += "|";
-        for (size_t column = 0; column < columns; column++) {
-            maze += "-";
-        }
-        maze += "|\n";
-    }
+
     return maze;
 }
 
